43afe0cdb95ccf1143a7d895225de1c2f18f8bf2
“Kaiwen <kevin9966hu@gmail.com>
2024-10-28 21:41:02 -0500

    add-image

---
+++ src/Image.js
@@ -0,0 +1,9 @@
+
+function Image() {
+    return (
+        <img class="top-img" src="https://images.pexels.com/photos/2696064/pexels-photo-2696064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+        </img>
+    );
+}
+
+export default Image
